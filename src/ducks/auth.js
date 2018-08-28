8bd401351c69278bb4c48cb8c3ddf1c265caeeb0
a.masalov <delsol@bk.ru>
2018-08-28 22:35:07 +0300

    remove thunk

--- src/ducks/auth.js
+++ src/ducks/auth.js
@@ -1,5 +1,7 @@
 import firebase from 'firebase';
 import { Record } from 'immutable';
+import { all, take, call, put, cps } from 'redux-saga/effects';
+
 import { appName } from '../config';
 
 const ReducerRecord = Record({
@@ -35,16 +37,39 @@ export default function reducer(state = new ReducerRecord(), action) {
 }
 
 export function signUp(email, password) {
-  return (dispatch) => {
-    dispatch({ type: SIGN_UP_REQUEST });
-    firebase
-      .auth()
-      .createUserWithEmailAndPassword(email, password)
-      .then((user) => {
-        dispatch({ type: SIGN_UP_SUCCESS, payload: { user } });
-      })
-      .catch((error) => {
-        dispatch({ type: SIGN_UP_ERROR, error });
-      });
-  };
+  return { type: SIGN_UP_REQUEST, payload: { email, password } };
 }
+
+export const signUpSaga = function*() {
+  const auth = firebase.auth();
+
+  while (true) {
+    const action = yield take(SIGN_UP_REQUEST);
+
+    try {
+      const user = yield call(
+        [auth, auth.createUserWithEmailAndPassword],
+        action.payload.email,
+        action.payload.password
+      );
+
+      yield put({ type: SIGN_UP_SUCCESS, payload: { user } });
+    } catch (error) {
+      yield put({ type: SIGN_UP_ERROR, error });
+    }
+  }
+};
+
+export const watchStatusChange = function*() {
+  const auth = firebase.auth();
+
+  try {
+    yield cps([auth, auth.onAuthStateChanged]);
+  } catch (user) {
+    yield put({ type: SIGN_UP_SUCCESS, payload: { user } });
+  }
+};
+
+export const saga = function*() {
+  yield all([signUpSaga(), watchStatusChange()]);
+};
--- src/redux/index.js
+++ src/redux/index.js
@@ -2,24 +2,22 @@ import { createStore, applyMiddleware } from 'redux';
 import { routerMiddleware } from 'react-router-redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { createLogger } from 'redux-logger';
-import thunk from 'redux-thunk';
 import createSagaMiddleware from 'redux-saga';
 
 import history from '../history';
 import reducer from './reducer';
-import { saga } from '../ducks/people';
+import rootSaga from './saga';
 
 const sagaMiddleware = createSagaMiddleware();
 
 const enhancer = applyMiddleware(
   sagaMiddleware,
   routerMiddleware(history),
-  thunk,
   createLogger({ collapsed: true })
 );
 
 const store = createStore(reducer, composeWithDevTools(enhancer));
 
-sagaMiddleware.run(saga);
+sagaMiddleware.run(rootSaga);
 
 export default store;

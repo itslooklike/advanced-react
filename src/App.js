31cfdb7f3951046f02c765c87b024f88a17d15ae
a.masalov <delsol@bk.ru>
2018-08-30 21:51:40 +0300

    virtualizer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import store from './redux';
 import Root from './components/Root';
--- src/components/events/EventList.js
+++ src/components/events/EventList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Table, Column } from 'react-virtualized';
+import 'react-virtualized/styles.css';
 
 import Spinner from '../common/Spinner';
 import { eventsFetch, selectEvent, eventsListSelector } from '../../ducks/events';
@@ -14,6 +16,15 @@ export class EventList extends Component {
     selectEvent && selectEvent(uid);
   };
 
+  onVirtualClickHandler = (rowData) => {
+    const { selectEvent } = this.props;
+    selectEvent && selectEvent(rowData.uid);
+  };
+
+  rowGetter = ({ index }) => {
+    return this.props.events[index];
+  };
+
   render() {
     const { events } = this.props;
 
@@ -23,15 +34,39 @@ export class EventList extends Component {
 
         {events.length ? (
           <div>
-            {events.map((event) => (
-              <div
-                key={event.uid}
-                className="test-list-row"
-                onClick={this.onClickHandler(event.uid)}
-              >
-                {event.title}
+            <div className="card">
+              <div className="card-header">Virtualize Table</div>
+              <div className="card-body">
+                <Table
+                  rowCount={events.length}
+                  rowGetter={this.rowGetter}
+                  rowHeight={40}
+                  headerHeight={50}
+                  width={700}
+                  height={100}
+                  onRowClick={this.onVirtualClickHandler}
+                >
+                  <Column label="title" dataKey="title" width={250} />
+                  <Column label="where" dataKey="where" width={250} />
+                </Table>
               </div>
-            ))}
+            </div>
+
+            <table className="table">
+              <tbody>
+                {events.map((event) => (
+                  <tr
+                    key={event.uid}
+                    className="test-list-row"
+                    onClick={this.onClickHandler(event.uid)}
+                  >
+                    <th scope="col">{event.title}</th>
+                    <th scope="col">{event.where}</th>
+                    <th scope="col">{event.when}</th>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
         ) : (
           <Spinner />
